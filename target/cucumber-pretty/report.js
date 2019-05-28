8bc615df8e66415848a4d60c690ee274325bca33
Khaja Hussain <poolakhaja@gmail.com>
2019-05-28 10:58:41 +0530

    added

--- target/cucumber-pretty/report.js
+++ target/cucumber-pretty/report.js
@@ -13,11 +13,11 @@ formatter.feature({
   ]
 });
 formatter.before({
-  "duration": 20839036322,
+  "duration": 37505825702,
   "status": "passed"
 });
 formatter.before({
-  "duration": 59658,
+  "duration": 132531,
   "status": "passed"
 });
 formatter.scenario({
@@ -92,58 +92,57 @@ formatter.match({
   "location": "HomepageSteps.launch_the_spicejet_application()"
 });
 formatter.result({
-  "duration": 2886987,
+  "duration": 2407637942,
   "status": "passed"
 });
 formatter.match({
   "location": "HomepageSteps.move_to_mouse_on_Login_SignUp_button()"
 });
 formatter.result({
-  "duration": 1027583449,
+  "duration": 1056800315,
   "status": "passed"
 });
 formatter.match({
   "location": "HomepageSteps.move_to_the_mouse_SpiceCash_SpiceClub_Member_button()"
 });
 formatter.result({
-  "duration": 721628161,
+  "duration": 734752888,
   "status": "passed"
 });
 formatter.match({
   "location": "LoginSteps.user_Navigate_to_LogIn_Page()"
 });
 formatter.result({
-  "duration": 10585880497,
-  "status": "passed"
+  "duration": 5829139537,
+  "error_message": "org.openqa.selenium.WebDriverException: unknown error: Element \u003ca href\u003d\"https://book.spicejet.com/Login.aspx\"\u003e...\u003c/a\u003e is not clickable at point (876, 80). Other element would receive the click: \u003ca class\u003d\"spiceFare\" href\u003d\"https://book.spicejet.com/Search.aspx?op\u003d4\" title\u003d\"Web Check-in\"\u003e...\u003c/a\u003e\n  (Session info: chrome\u003d74.0.3729.169)\n  (Driver info: chromedriver\u003d2.40.565498 (ea082db3280dd6843ebfb08a625e3eb905c4f5ab),platform\u003dWindows NT 10.0.15063 x86_64) (WARNING: The server did not provide any stacktrace information)\nCommand duration or timeout: 0 milliseconds\nBuild info: version: \u00273.7.0\u0027, revision: \u00272321c73\u0027, time: \u00272017-11-02T22:22:35.584Z\u0027\nSystem info: host: \u0027LP-5CD7465GP0\u0027, ip: \u0027172.23.0.1\u0027, os.name: \u0027Windows 10\u0027, os.arch: \u0027amd64\u0027, os.version: \u002710.0\u0027, java.version: \u00271.8.0_171\u0027\nDriver info: org.openqa.selenium.chrome.ChromeDriver\nCapabilities {acceptInsecureCerts: false, acceptSslCerts: false, applicationCacheEnabled: false, browserConnectionEnabled: false, browserName: chrome, chrome: {chromedriverVersion: 2.40.565498 (ea082db3280dd6..., userDataDir: C:\\Users\\khaja.h\\AppData\\Lo...}, cssSelectorsEnabled: true, databaseEnabled: false, handlesAlerts: true, hasTouchScreen: false, javascriptEnabled: true, locationContextEnabled: true, mobileEmulationEnabled: false, nativeEvents: true, networkConnectionEnabled: false, pageLoadStrategy: normal, platform: XP, platformName: XP, rotatable: false, setWindowRect: true, takesHeapSnapshot: true, takesScreenshot: true, unexpectedAlertBehaviour: , unhandledPromptBehavior: , version: 74.0.3729.169, webStorageEnabled: true}\nSession ID: ec0ebb41d3c5534b15ce523ecac5c3df\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance(Unknown Source)\r\n\tat sun.reflect.DelegatingConstructorAccessorImpl.newInstance(Unknown Source)\r\n\tat java.lang.reflect.Constructor.newInstance(Unknown Source)\r\n\tat org.openqa.selenium.remote.ErrorHandler.createThrowable(ErrorHandler.java:214)\r\n\tat org.openqa.selenium.remote.ErrorHandler.throwIfResponseFailed(ErrorHandler.java:166)\r\n\tat org.openqa.selenium.remote.http.JsonHttpResponseCodec.reconstructValue(JsonHttpResponseCodec.java:40)\r\n\tat org.openqa.selenium.remote.http.AbstractHttpResponseCodec.decode(AbstractHttpResponseCodec.java:80)\r\n\tat org.openqa.selenium.remote.http.AbstractHttpResponseCodec.decode(AbstractHttpResponseCodec.java:44)\r\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:164)\r\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.execute(DriverCommandExecutor.java:83)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:600)\r\n\tat org.openqa.selenium.remote.RemoteWebElement.execute(RemoteWebElement.java:279)\r\n\tat org.openqa.selenium.remote.RemoteWebElement.click(RemoteWebElement.java:83)\r\n\tat sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)\r\n\tat sun.reflect.NativeMethodAccessorImpl.invoke(Unknown Source)\r\n\tat sun.reflect.DelegatingMethodAccessorImpl.invoke(Unknown Source)\r\n\tat java.lang.reflect.Method.invoke(Unknown Source)\r\n\tat org.openqa.selenium.support.pagefactory.internal.LocatingElementHandler.invoke(LocatingElementHandler.java:51)\r\n\tat com.sun.proxy.$Proxy21.click(Unknown Source)\r\n\tat com.cucumber.PageObjects.LoginPageObjects.Member_Login_button(LoginPageObjects.java:95)\r\n\tat com.cucumber.stepdefination.LoginSteps.user_Navigate_to_LogIn_Page(LoginSteps.java:26)\r\n\tat ✽.When User Navigate to LogIn Page(src/test/resources/features/Login.feature:8)\r\n",
+  "status": "failed"
 });
 formatter.match({
   "location": "LoginSteps.user_enters_Credentials_to_LogIn(DataTable)"
 });
 formatter.result({
-  "duration": 31314678927,
-  "status": "passed"
+  "status": "skipped"
 });
 formatter.match({
   "location": "LoginSteps.message_displayed_Login_Successfully()"
 });
 formatter.result({
-  "duration": 104968,
+  "status": "skipped"
+});
+formatter.after({
+  "duration": 977325108,
   "status": "passed"
 });
 formatter.after({
-  "duration": 70230,
+  "duration": 115163,
   "status": "passed"
 });
 formatter.after({
-  "duration": 58903,
+  "duration": 1380373549,
   "status": "passed"
 });
 formatter.after({
-  "duration": 3275357608,
-  "status": "passed"
-});
-formatter.after({
-  "duration": 66454,
+  "duration": 1457841,
   "status": "passed"
 });
 });
