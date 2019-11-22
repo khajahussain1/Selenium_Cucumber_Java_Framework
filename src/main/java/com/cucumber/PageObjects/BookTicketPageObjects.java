568e43f88cc507ef6cb584eafbf754de6352c0a4
Khaja Hussain <poolakhaja@gmail.com>
2019-11-22 21:03:22 +0530

    commit all files

--- src/main/java/com/cucumber/PageObjects/BookTicketPageObjects.java
+++ src/main/java/com/cucumber/PageObjects/BookTicketPageObjects.java
@@ -9,41 +9,36 @@ import com.cucumber.TestBase.TestBase;
 
 public class BookTicketPageObjects {
 
-	
 	WebDriver driver;
 	TestBase testBase;
-	
-	public BookTicketPageObjects(WebDriver driver)
-	{
+
+	public BookTicketPageObjects(WebDriver driver) {
 		this.driver = driver;
 		PageFactory.initElements(driver, this);
 		testBase = new TestBase(driver);
+
 	}
-	
-	@FindBy(xpath="//input[@id='ctl00_mainContent_ddl_originStation1_CTXT']")
+
+	@FindBy(xpath = "//input[@id='ctl00_mainContent_ddl_originStation1_CTXT']")
 	public WebElement from_dropdon;
-	
+
 	@FindBy(xpath = "//a[text()=' Bengaluru (BLR)']")
 	public WebElement from_dropdon_value;
-	
+
 	@FindBy(linkText = "Hyderabad (HYD)")
 	public WebElement to_drop_down_value;
-	
-	
-	public void select_from_dropdown()
-	{
+
+	public void select_from_dropdown() {
 		testBase.WaiteForElement(from_dropdon);
 		from_dropdon.click();
 	}
-	
-	public void select_from_dropdown_value()
-	{
+
+	public void select_from_dropdown_value() {
 		testBase.WaiteForElement(from_dropdon_value);
 		from_dropdon_value.click();
 	}
-	
-	public void select_to_dropdown_value()
-	{
+
+	public void select_to_dropdown_value() {
 		testBase.WaiteForElement(to_drop_down_value);
 		to_drop_down_value.click();
 	}
